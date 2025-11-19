764fe033949aa97d49d41931476ada69d90818da
Alessio Ceccarini <alessioceccarini23@icloud.com>
2025-11-19 16:15:38 +0100

    adds

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import FooterPage from "./Components/FooterPage"
 // import AllTheBooks from "./Components/AllTheBooks"
 import history from "./Data/history.json"
 import BookList from "./Components/BookList"
+import AlertPage from "./Components/AlertPage"
 function App() {
   return (
     <>
       <HeaderPage />
+      <AlertPage />
       <BookList books={history} />
       {/* <AllTheBooks /> */}
       <FooterPage />
